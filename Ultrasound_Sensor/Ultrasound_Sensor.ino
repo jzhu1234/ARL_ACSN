f46db51e26be53e8eafff29e6b7ac623ff9074b0
Quan7968 <pquan1@vt.edu>
2017-07-25 14:32:35 -0400

    Add files via upload

---
+++ Ultrasound_Sensor/Ultrasound_Sensor.ino
@@ -0,0 +1,33 @@
+const int trigPin = 9;
+const int echoPin = 10;
+
+// defines variables
+long duration;
+int distance;
+
+void setup() {
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  Serial.begin(9600); //Starts the serial communication
+}
+
+void loop() {
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delay(2000);
+
+  // Sets the trigPin on HIGH state for 10 microseconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+
+  // Calculating the distance
+  distance = duration*0.034/2;
+
+  //Prints the distance on the serial monitor
+  Serial.write(0x20);
+  Serial.write(distance);
+}
