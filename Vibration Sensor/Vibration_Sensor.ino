7467bbad61a4623f49327f545b3c10bd227e8e29
shreinhart <30445061+shreinhart@users.noreply.github.com>
2017-08-02 09:11:31 -0400

    Update Vibration_Sensor.ino

--- Vibration Sensor/Vibration_Sensor.ino
+++ Vibration Sensor/Vibration_Sensor.ino
@@ -78,6 +78,26 @@ void setup()
 
 void loop() 
 {
+  while (Serial.available >= 3) {
+    byte h = Serial.read();
+    byte m = Serial.read();
+    byte l = Serial.read();
+    if (h == 0) {
+      Serial.print("+++");
+      delay(1000);
+      Serial.print("ATID");
+      Serial.print(m, HEX);
+      Serial.print(l, HEX);
+      Serial.print('/r');
+      delay(1000);
+      Serial.print("ATWR/r");
+      while (Serial.available >= 3) {
+        Serial.read();
+        Serial.read();
+        Serial.read();
+      }
+    }
+  }
   if(f_timer==1){
     f_timer = 0;
     // Read Piezo ADC value in, and convert it to a voltage
